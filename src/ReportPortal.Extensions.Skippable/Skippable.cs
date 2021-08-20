7828f9a06d3a69b86d90613b35b64b8d56f8b7f1
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2021-08-20 16:23:26 +0300

    Initial version

---
+++ src/ReportPortal.Extensions.Skippable/Skippable.cs
@@ -0,0 +1,25 @@
+﻿using ReportPortal.Client.Abstractions.Models;
+using ReportPortal.Shared.Extensibility;
+using ReportPortal.Shared.Extensibility.ReportEvents;
+
+namespace ReportPortal.Extensions.Skippable
+{
+    public class Skippable : IReportEventsObserver
+    {
+        public void Initialize(IReportEventsSource reportEventsSource)
+        {
+            reportEventsSource.OnBeforeTestFinishing += ReportEventsSource_OnBeforeTestFinishing;
+        }
+
+        private void ReportEventsSource_OnBeforeTestFinishing(Shared.Reporter.ITestReporter testReporter, Shared.Extensibility.ReportEvents.EventArgs.BeforeTestFinishingEventArgs args)
+        {
+            if (args.FinishTestItemRequest.Status == Status.Skipped)
+            {
+                args.FinishTestItemRequest.Issue = new Client.Abstractions.Responses.Issue
+                {
+                    Type = WellKnownIssueType.NotDefect
+                };
+            }
+        }
+    }
+}
