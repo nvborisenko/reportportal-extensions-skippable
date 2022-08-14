2e9bc672278b6e4d475967a2b8147c751ac05561
Aleh Yanushkevich <Aleh_Yanushkevich2@epam.com>
2022-08-14 09:42:25 +0300

    Added skipp mime type functionality.

---
+++ src/ReportPortal.Extensions.Skippable/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using ReportPortal.Shared.Configuration;
+
+namespace ReportPortal.Extensions.Skippable.Extensions
+{
+    internal static class ConfigurationExtensions
+    {
+        private const char ArrayItemsDelimiter = ';';
+
+        public static string[] GetSkippableMimeTypes(this IConfiguration configuration)
+        {
+            return configuration
+                .GetValue("Extensions:Skippable:MimeTypes", string.Empty)
+                .Split(ArrayItemsDelimiter);
+        }
+    }
+}
--- src/ReportPortal.Extensions.Skippable/Skippable.cs
+++ src/ReportPortal.Extensions.Skippable/Skippable.cs
@@ -1,6 +1,10 @@
 ﻿using ReportPortal.Client.Abstractions.Models;
+using ReportPortal.Extensions.Skippable.Extensions;
 using ReportPortal.Shared.Extensibility;
 using ReportPortal.Shared.Extensibility.ReportEvents;
+using ReportPortal.Shared.Extensibility.ReportEvents.EventArgs;
+using ReportPortal.Shared.Reporter;
+using System.Linq;
 
 namespace ReportPortal.Extensions.Skippable
 {
@@ -9,9 +13,28 @@ namespace ReportPortal.Extensions.Skippable
         public void Initialize(IReportEventsSource reportEventsSource)
         {
             reportEventsSource.OnBeforeTestFinishing += ReportEventsSource_OnBeforeTestFinishing;
+            reportEventsSource.OnBeforeLogsSending += ReportEventsSource_OnBeforeLogsSending;
         }
 
-        private void ReportEventsSource_OnBeforeTestFinishing(Shared.Reporter.ITestReporter testReporter, Shared.Extensibility.ReportEvents.EventArgs.BeforeTestFinishingEventArgs args)
+        private void ReportEventsSource_OnBeforeLogsSending(ILogsReporter logsReporter, BeforeLogsSendingEventArgs args)
+        {
+            var skippableMimeTypes = args.Configuration.GetSkippableMimeTypes();
+
+            if (!skippableMimeTypes.Any())
+            {
+                return;
+            }
+
+            foreach (var request in args.CreateLogItemRequests.Where(request => request.Attach != null))
+            {
+                if (skippableMimeTypes.Contains(request.Attach.MimeType))
+                {
+                    request.Attach = null;
+                }
+            }
+        }
+
+        private void ReportEventsSource_OnBeforeTestFinishing(ITestReporter testReporter, BeforeTestFinishingEventArgs args)
         {
             if (args.FinishTestItemRequest?.Status == Status.Skipped)
             {
